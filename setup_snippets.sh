b1c4ff9d48e58e79ade3b9929ae200f190276b30
Emir <ccoding2015@hotmail.com>
2016-11-18 17:41:01 +0800

    ln Xcode CodeSnippets

---
+++ setup_snippets.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
+
+SRC_HOME=`pwd`
+ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+echo "done"
+
